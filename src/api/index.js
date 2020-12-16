2cafe5d46509e2bb5d76c52c96f1423f8e629193
Bilal Iqbal <muhammadbilaliqbal98@gmail.com>
2020-12-17 00:48:49 +0500

    adding features
    adding features

--- src/api/index.js
+++ src/api/index.js
@@ -7,7 +7,7 @@ const url = "https://covid19.mathdro.id/api"
 // asyncronise data read and write data
 // try successful hoga jub warna catch me ajega 
 // agar data nhi mila to 
-export const fetchData = async()=>{
+ const fetchData = async()=>{
     try {
         const {data: {confirmed, recovered, deaths, lastUpdate}} = await axios.get(url)
         // hamain api pe data mese data jis ki hamain zarorat he bus
@@ -21,6 +21,37 @@ export const fetchData = async()=>{
 
         return {confirmed, recovered, deaths, lastUpdate};
     } catch (error) {
-        
+        console.log(error)
     }
+}
+ const fetchDailyData = async()=>{
+    try {
+        const {data } = await axios.get(`${url}/daily`)
+        // console.log(data)
+        const modifiedData = data.map((dailyData)=>({
+            confirmed: dailyData.confirmed.total,
+            recovered: dailyData.recovered.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate,
+        }));
+        return modifiedData;
+    } catch (error) {
+        console.log(error)
+
+    }
+}
+const fetchCountries = async()=>{
+    try {
+        const {data :{countries}} = await axios.get(`${url}/countries`)
+        // console.log(response)
+        return countries.map((country)=> country.name)
+    } catch (error) {
+        console.log(error)
+
+    }
+}
+export {
+    fetchData,
+    fetchDailyData,
+    fetchCountries,
 }
